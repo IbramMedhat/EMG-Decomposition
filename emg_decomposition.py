22ca0f8d29f5fa034caca0b57edd4447859a6b64
Basem Rizk <basem.rizk@outlook.com>
2020-04-18 16:05:32 +0200

    muap spikes enhanced detection

--- emg_decomposition.py
+++ emg_decomposition.py
@@ -28,7 +28,7 @@ def get_muaps_timestamps(sig,
                          verbose=False):
     def find_noise(sig):
         # Noise is any part of the signal that does contain MUAPs
-        return sig[:120]
+        return sig[:110]
     
     def apply_moving_avg(sig, win_size=20):
         smoothed_signal = np.zeros(sig.shape)
@@ -44,19 +44,47 @@ def get_muaps_timestamps(sig,
                                 discard_margin=20):
         values_indices = set()
         step_of_first_observation = None
-    
+        adding_margin = discard_margin/2
         v_i = 0
-        while(v_i < sig.shape[0]):
+        while v_i < sig.shape[0] :
             v = sig[v_i]
             if v >= threshold:
                 if step_of_first_observation is None:
-                    step_of_first_observation = v_i
-                    v_i += discard_margin
-                    continue
+                    step_of_first_observation = v_i 
+                    # v_i += discard_margin
+                    # print("\nverify a spike")
+                    consecutive_aboves = []
+                    v_i_tmp = v_i + 1
+                    while v_i < sig.shape[0]:
+                        if sig[v_i] < threshold:
+                            break
+                        consecutive_aboves.append(sig[v_i])
+                        v_i += 1
+                    consecutive_aboves = np.array(consecutive_aboves)
+                    # print(consecutive_aboves)
+                    # Using first max. value occurance shifted back with
+                    # window half size
+                    step_of_first_observation =\
+                        np.argmax(consecutive_aboves) +\
+                        step_of_first_observation -\
+                        adding_margin
+                        
+                    if consecutive_aboves.shape[0] < discard_margin:
+                        v_i = v_i_tmp      
+                        step_of_first_observation = None
+
+                        
+                    # for v_2i in range(step_of_first_observation, v_i):
+                    #     # print("@v_2i =  %s, v = %s" % (v_2i, sig[v_2i]) )
+                    #     if (v_2i < sig.shape[0]) and (sig[v_2i] < threshold):
+                    #         step_of_first_observation = None
+                    #         v_i = v_2i + 1
+                    #         break
+                    # continue
             else:
                 step_of_first_observation = None
             if step_of_first_observation is not None:
-                values_indices.add(step_of_first_observation)
+                values_indices.add(int(step_of_first_observation))
             v_i += 1
 
         values_indices = list(values_indices)
@@ -71,24 +99,26 @@ def get_muaps_timestamps(sig,
     moving_avg_signal = apply_moving_avg(rectified_signal,
                                          win_size=moving_avg_win_size)
     if verbose:
-        plot(moving_avg_signal, title="moving_avg_signal")
+        plot(moving_avg_signal, title="moving_avg_signal",
+             begin=0, end=-1)
     
     noise = find_noise(rectified_signal)
     threshold = 3*np.std(noise)
+    
+    print("threshold value = %s" % threshold)
+    # threshold = 11.7
     timestamps = define_above_thresholds(moving_avg_signal,
                                          threshold,
                                          discard_margin=moving_avg_win_size)
     if verbose:
         plot(raw_signal, title="Marked MUAPs", markers=timestamps,
-             begin=2200, end=3200)
+             begin=0, end=-1)
         
     return timestamps
 
 def define_muaps_templates(raw_signal, timestamps,
                             diffTh, moving_avg_win_size,
-                            verbose=False):
-    # TODO STEP 2 complete
-    
+                            verbose=False):    
     # m: muap; k: template
     
     def sqr_diff(m, k):
@@ -109,19 +139,21 @@ def define_muaps_templates(raw_signal, timestamps,
         merged = False
         for j, k in enumerate(templates):  
             diff = sqr_diff(m, k)
-            print(diff)
             if (diff < diffTh):
                 #  m is part of k_i
+                # print("Merge spike %s with template %s" % (i,j))
                 templates[j] = update_template(m, k)
                 merged = True
                 break
         if not merged:
             templates.append(m)
+            print("Spike %s makes new template %s" % (i, len(templates) -1))
+
     
     templates = np.array(templates)
     
     if verbose:
-        for i, template in enumerate(templates[:3]):
+        for i, template in enumerate(templates):
             plot(template, title="MUAP " + str(i))
         
     return templates
@@ -131,7 +163,7 @@ def define_muaps_templates(raw_signal, timestamps,
 def plot(sig, title = "Plot of CT signal", sampling_rate=1,
          xlabel="t", ylabel="x(t)", markers=None, save=False,
          begin=0, end=1000):
-    if end == 0 | end > sig.shape[0]:
+    if end <= 0 or end > sig.shape[0]:
         end = sig.shape[0]
         
     draw_sig=sig[begin:end]
@@ -154,11 +186,20 @@ def plot(sig, title = "Plot of CT signal", sampling_rate=1,
 
 if __name__ == '__main__':
     moving_avg_win_size=20
-    diffTh=1265000
+    diffTh=1275000
     raw_signal = pd.read_csv("Data.txt", header=None)[0]
+    raw_signal = raw_signal.reset_index()[0]
     timestamps, templates = decompose(raw_signal,
                                       moving_avg_win_size=moving_avg_win_size,
                                       diffTh=diffTh, verbose=True)
     print("%s timstamps are detected, reduced to %s templates"\
           % (timestamps.shape[0], templates.shape[0]))
  
+    # TODO A figure showing from sample 30000 to sample 35000 of the EMG signal
+    # with an “*” marking the detected MUAPs colored with different colors
+    # depending on the MU each MUAP belongs to (Similar to slide 19). 
+    # Name the figure “DetectedMUAP.jpg”
+
+    
+    # TODO A figure showing the waveform of each template of the detected MUs
+    # (Similar to slide 20). Name the figure “Templates.jpg”
